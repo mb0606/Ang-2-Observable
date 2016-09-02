6ff6b9109805d20097742b46fa35f9d132e7dacb
Marco Berardini <mberardini@evolution-man.com>
2016-09-02 12:01:07 -0700

    [client] Refactor using Observables

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,23 +11,40 @@ import {Observable} from 'rxjs/Rx';
 })
 export class AppComponent {
     constructor(){
+        //
+        var keyups = Observable.fromEvent($("#search"), "keyup")
+            .map( (e) => e.target.value)
+            .filter( (text) => text.length >= 3 )
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .flatMap(searchTerm => {
+                var url = "https:api.spotify.com/v1/search?type=artist&q=" + searchTerm
+                var promise = $.getJSON(url);
+                return Observable.fromPromise(promise);
+            });
 
-        var debounced = _.debounce(function(text){
-            $.getJSON(url, function (artists) {
-                console.log(artists);
-            })
+        keyups.subscribe((data) => {
+            console.log(data);
+        })
 
-        }, 400);
 
-        var url = "https:api.spotify.com/v1/search?type=artist&q=" + text
-        $("#search").keyup(function(e){
-            var text = e.target.value;
 
-            if(text.length < 3) {
-                return;
-            }
-
-            debounced(text);
-        });
+        // var debounced = _.debounce(function(text){
+        // var url = "https:api.spotify.com/v1/search?type=artist&q=" + text
+        //     $.getJSON(url, function (artists) {
+        //         console.log(artists);
+        //     })
+        //
+        // }, 400);
+        //
+        // $("#search").keyup(function(e){
+        //     var text = e.target.value;
+        //
+        //     if(text.length < 3) {
+        //         return;
+        //     }
+        //
+        //     debounced(text);
+        // });
     }
 }
